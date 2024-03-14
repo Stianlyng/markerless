4d746e5a1bcf86a4928066936f76dae9d3fb9de5
Stian Lyng <stianlyng@protonmail.com>
2024-03-14 15:29:34 +0100

    remove cameramanager

--- CameraManager/flycapture/camera_manager/src/cameraproperty.cpp
+++
@@ -1,8 +0,0 @@
-#include "cameraproperty.h"
-#include <QDebug>
-
-using namespace CameraManager;
-
-CameraProperty::CameraProperty(CameraManager::PropertyType type, QString name, double min, double max, int decimals, bool canAuto, bool isAuto, bool onOff, double value)
-    : type(type), name(name), min(min), max(max), decimals(decimals), canAuto(canAuto), isAuto(isAuto), onOff(onOff), val(value) {
-}
--- CameraManager/flycapture/camera_manager/src/ui_mainwindow.cpp
+++
@@ -1,3 +0,0 @@
-
-
-#include "ui_mainwindow.h"
